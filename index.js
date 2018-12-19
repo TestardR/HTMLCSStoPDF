456839827c23d6c3048ac789df5f805de6aa5661
Romain Testard <40339476+TestardR@users.noreply.github.com>
2018-12-19 10:48:37 +0100

    Update index.js

--- index.js
+++ index.js
@@ -3,15 +3,14 @@ const puppeteer = require('puppeteer');
 (async () => {
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
-  await page.goto(`http://127.0.0.1:5500/index.html`, {
+  await page.goto(`filelocation/index.html`, {
     waitUntil: 'networkidle2'
   });
   await page.emulateMedia('print');
   await page.pdf({
-    path: 'cv3.pdf',
+    path: 'index.pdf',
     format: 'A4',
     printBackground: true
-    // displayHeaderFooter: true
   });
   await browser.close();
 })();
